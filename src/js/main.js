e631b8979ec5f2eb94be7c4abb43c799f3e36efa
JDC <jdcdevelopmenrcompany@gmail.com>
2017-11-16 12:48:48 +0200

    Develop lending

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,56 @@ function autoSlider(){
         autoSlider();  
     },3000);
 }
+
+autoSlider2();
+var left2 = 0;
+function autoSlider2(){
+    var timer2 = setTimeout(function(){
+      var sliderWidth2 = document.getElementById('slider-hover');
+        left2 = left2 - 578;
+        if (left2 < -578){
+            left2 = 578;
+            clearTimeout(timer2);
+        }
+        sliderWidth2.style.left = left2 + 'px';
+        autoSlider2();  
+    },3000);
+}
+
+$(document).ready(function(){
+    $('.fa-heart-o').hover(function(){
+        $('#image-hover').removeClass('fa fa-briefcase fa-5x');
+        $('#image-hover').removeClass('fa fa-leaf fa-5x');
+        $('#image-hover').removeClass('fa fa-microchip fa-5x');
+        $('#image-hover').removeClass('fa fa-umbrella fa-5x');
+        $('#image-hover').addClass('fa fa-heart-o fa-5x');  
+    });
+    $('.fa-briefcase').hover(function(){
+        $('#image-hover').removeClass('fa fa-heart-o fa-5x');
+        $('#image-hover').removeClass('fa fa-leaf fa-5x');
+        $('#image-hover').removeClass('fa fa-microchip fa-5x');
+        $('#image-hover').removeClass('fa fa-umbrella fa-5x');
+        $('#image-hover').addClass('fa fa-briefcase fa-5x');
+    });
+     $('.fa-leaf').hover(function(){
+        $('#image-hover').removeClass('fa fa-briefcase fa-5x');
+        $('#image-hover').removeClass('fa fa-heart-o fa-5x');
+        $('#image-hover').removeClass('fa fa-microchip fa-5x');
+        $('#image-hover').removeClass('fa fa-umbrella fa-5x');
+        $('#image-hover').addClass('fa fa-leaf fa-5x');  
+    });
+    $('.fa-microchip').hover(function(){
+        $('#image-hover').removeClass('fa fa-heart-o fa-5x');
+        $('#image-hover').removeClass('fa fa-leaf fa-5x');
+        $('#image-hover').removeClass('fa fa-briefcase fa-5x');
+        $('#image-hover').removeClass('fa fa-umbrella fa-5x');
+        $('#image-hover').addClass('fa fa-microchip fa-5x');
+    });
+    $('.fa-umbrella').hover(function(){
+        $('#image-hover').removeClass('fa fa-heart-o fa-5x');
+        $('#image-hover').removeClass('fa fa-leaf fa-5x');
+        $('#image-hover').removeClass('fa fa-briefcase fa-5x');
+        $('#image-hover').removeClass('fa fa-microchip fa-5x');
+        $('#image-hover').addClass('fa fa-umbrella fa-5x');
+    });
+});
